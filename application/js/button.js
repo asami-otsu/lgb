670016f15a4857992f691d9242f12516ebb7b9d1
hiyucc <hiyu7326@gmail.com>
2015-09-02 13:01:54 +0900

    add manager

---
+++ application/js/button.js
@@ -0,0 +1,83 @@
+
+/**
+ * LGBボタン基本オブジェクト
+ * @param int x
+ * @param int y
+ * @param object params
+ */
+lgb.button = function(x, y, params){
+	this.position_x = x || 0;
+	this.position_y = y || 0;
+	this.text = params.text || "NoneText";
+	this.size_x = params.size_x || 400;
+	this.size_y = params.size_y || 30;
+	this.inOverMouse = false;
+
+	this.canvas = null;
+	this.ctx = null;
+	this.c_rect = null;
+
+	this.init = function(){
+		this.canvas = $('#'+this.text).get(0);
+		if( this.canvas.getContext ){
+			this.ctx = this.canvas.getContext('2d');
+			lgb.init(this.ctx);
+		}
+		this.c_rect = $(this.canvas).offset();
+
+		// addEventListener
+		this.canvas.addEventListener('mouseover', this.onOver, false);
+		this.canvas.addEventListener('click', this.onClick, false);
+		this.canvas.addEventListener('mouseout', this.onOut, false);
+	};
+
+	this.draw = function(x, y, text){
+		var x = x || this.position_x;
+		var y = y || this.position_y;
+		var text = text || this.text;
+
+		// ctx init
+		lgb.ctx_init(this.ctx);
+
+		// 描画
+		this.ctx.fillText(this.text, 0, 0);
+
+		if( this.isOverMouse ){
+			this.ctx.strokeStyle = '#ff0000';
+		}
+		this.ctx.strokeRect(0, 0, this.size_x, this.size_y);
+	};
+
+	this.finish = function(){
+console.log(this.text + " is finish");
+		$(this.text).remove();
+	};
+
+	this.drawDisable = function(){
+	};
+
+	this.onClick = function(e){
+console.log(this.text + " onClick");
+		// すでにのっている状態
+	};
+
+	this.onOver = function(e){
+console.log(this.text + " onOver");
+		this.isOverMouse = true;
+	};
+	
+	this.onOut = function(e){
+console.log(this.text + " onOut");
+		this.isOverMouse = false;
+	};
+
+	this.isCollision = function(m_x, m_y){
+		var x = this.position_x + this.c_rect.left;
+		var y = this.position_y + this.c_rect.top;
+		return (x <= m_x && y <= m_y && (x + this.size_x) >= m_x && (y + this.size_y) >= m_y );
+	};
+
+	// canvasに自分要素用のcanvas作成
+	$(canvas).append("<canvas id='"+this.text+"' width='"+this.size_x+"' height='"+this.size_y+"' style='background-color: #ff00ff' ></canvas>");
+};
+
